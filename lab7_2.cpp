1f733ab0283795b3ffe3c47f2eccaccfa50988a9
Nathanon-Jaem <kumch29@gmail.com>
2025-12-16 20:24:33 +0700

    Implement gear calculation and user interaction
    Added a function to calculate the gear based on student ID and updated the main dialogue to include user input for name, student ID, movie, and date.

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -1,18 +1,61 @@
-//Dialog of the first example is given below.
+#include <iostream>
+#include <string>
+#include <sstream>
 
-/*
-Fahsai: Sawadee ka...Can you tell me your name?
-?????: Luke Skywalker
-Fahsai: Wow!!! Luke Skywalker is a really cool name.
-Fahsai: I think you are an Engineering student. What is your student ID?
-Luke Skywalker: 590610999
-Fahsai: I think you may be GEAR 47. I have a free movie ticket for you.
-Fahsai: Let's go to the cinema together!!!
-Fahsai: What movie do you want to watch?
-Luke Skywalker: Star Wars VII
-Fahsai: So....which day are you free to go with me?
-Luke Skywalker: Next Monday
-Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars VII with you.
-Luke Skywalker: May the Force be with you krub
-Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
-*/
+int calculateGear(const std::string& studentID) {
+    if (studentID.length() >= 2) {
+        std::string twoDigits = studentID.substr(0, 2);
+        
+        int year;
+        std::stringstream ss(twoDigits);
+        ss >> year;
+
+        return year - 12;
+    }
+    return 0;
+}
+
+int main() {
+    std::string name;
+    std::string studentID;
+    std::string movie;
+    std::string date;
+    std::string finalMessage;
+    int gear;
+
+    std::cout << "Fahsai: Sawadee ka...Can you tell me your name?\n";
+    std::cout << "?????: ";
+    std::getline(std::cin, name); 
+    
+    std::cout << "Fahsai: Wow!!! " << name << " is a really cool name.\n";
+    
+    std::cout << "Fahsai: I think you are an Engineering student. What is your student ID?\n";
+    
+    std::cout << name << ": ";
+    std::cin >> studentID;
+    
+    gear = calculateGear(studentID); 
+    std::cout << "Fahsai: I think you may be GEAR " << gear << ". I have a free movie ticket for you.\n";
+    
+    std::cout << "Fahsai: Let's go to the cinema together!!!\n";
+
+    std::cin.ignore(); 
+    
+    std::cout << "Fahsai: What movie do you want to watch?\n";
+    std::cout << name << ": ";
+    std::getline(std::cin, movie);
+    
+    std::cout << "Fahsai: So....which day are you free to go with me?\n";
+    std::cout << name << ": ";
+    std::getline(std::cin, date);
+
+    std::cout << "Fahsai: " << date << "....that is OK!!! I'm looking forward to watching\n";
+    std::cout << movie << " with you.\n";
+    
+    std::cout << name << ": ";
+    std::getline(std::cin, finalMessage);
+    
+    std::cout << "Fahsai: 555+ see you " << date << ". Bye Bye \\(^ ^)/\n";
+    
+    return 0;
+}
